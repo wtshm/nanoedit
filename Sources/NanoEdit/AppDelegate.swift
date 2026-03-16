6a9081868119b2e7697195848cbd4b8aed1a19d0
Kenta Watashima <kenta@achr.me>
2026-03-16 10:49:52 +0900

    Add AppDelegate with window lifecycle management

---
+++ Sources/NanoEdit/AppDelegate.swift
@@ -0,0 +1,63 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let filePath: String
+    private var window: EditorWindow?
+
+    init(filePath: String) {
+        self.filePath = filePath
+        super.init()
+    }
+
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+        setupMainMenu()
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let viewController = EditorViewController(filePath: filePath)
+        let window = EditorWindow(contentViewController: viewController)
+        window.makeKeyAndOrderFront(nil)
+        self.window = window
+
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    func applicationShouldTerminateAfterLastWindowIsClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+    private func setupMainMenu() {
+        let mainMenu = NSMenu()
+
+        // App menu
+        let appMenuItem = NSMenuItem()
+        let appMenu = NSMenu()
+        appMenu.addItem(withTitle: "Quit nanoedit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        appMenuItem.submenu = appMenu
+        mainMenu.addItem(appMenuItem)
+
+        // File menu
+        let fileMenuItem = NSMenuItem()
+        let fileMenu = NSMenu(title: "File")
+        fileMenu.addItem(withTitle: "Save", action: #selector(EditorViewController.saveAndExit), keyEquivalent: "s")
+        fileMenu.addItem(withTitle: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w")
+        fileMenuItem.submenu = fileMenu
+        mainMenu.addItem(fileMenuItem)
+
+        // Edit menu (required for standard text editing shortcuts)
+        let editMenuItem = NSMenuItem()
+        let editMenu = NSMenu(title: "Edit")
+        editMenu.addItem(withTitle: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
+        editMenu.addItem(NSMenuItem.separator())
+        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+        editMenuItem.submenu = editMenu
+        mainMenu.addItem(editMenuItem)
+
+        NSApp.mainMenu = mainMenu
+    }
+}
