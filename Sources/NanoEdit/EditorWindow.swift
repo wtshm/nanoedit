7deef8ecb469208cc538f366aa3136f99e44409a
Kenta Watashima <kenta@achr.me>
2026-03-16 10:50:01 +0900

    Add EditorWindow with floating level and frame autosave

---
+++ Sources/NanoEdit/EditorWindow.swift
@@ -0,0 +1,30 @@
+import AppKit
+
+class EditorWindow: NSWindow {
+    init(contentViewController viewController: NSViewController) {
+        let defaultFrame = NSRect(x: 0, y: 0, width: 600, height: 400)
+
+        super.init(
+            contentRect: defaultFrame,
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+
+        self.contentViewController = viewController
+        self.level = .floating
+        self.isReleasedWhenClosed = false
+        self.setFrameAutosaveName("NanoEditWindow")
+
+        // Set title to filename
+        if let viewController = viewController as? EditorViewController {
+            let url = URL(fileURLWithPath: viewController.filePath)
+            self.title = url.lastPathComponent
+        }
+
+        // Center only if no saved frame exists
+        if !self.setFrameUsingName("NanoEditWindow") {
+            self.center()
+        }
+    }
+}
