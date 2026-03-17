fa19a4bb28a5c625ef1ba26d72806a2ecac30697
Kenta Watashima <kenta@achr.me>
2026-03-17 21:57:11 +0900

    Rename target to NanoEdit and remove tests

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 let package = Package(
     name: "nanoedit",
     platforms: [.macOS(.v13)],
+    products: [
+        .executable(
+            name: "nanoedit",
+            targets: ["NanoEdit"]
+        ),
+    ],
     dependencies: [
         .package(url: "https://github.com/smittytone/HighlighterSwift.git", from: "3.0.0"),
     ],
     targets: [
         .executableTarget(
-            name: "nanoedit",
+            name: "NanoEdit",
             dependencies: [.product(name: "Highlighter", package: "highlighterswift")],
-            path: "Sources/nanoedit"
-        ),
-        .testTarget(
-            name: "nanoeditTests",
-            dependencies: ["nanoedit"],
-            path: "Tests/nanoeditTests"
+            path: "Sources/NanoEdit"
         ),
     ]
 )
